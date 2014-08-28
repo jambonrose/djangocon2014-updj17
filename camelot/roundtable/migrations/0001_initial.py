a83ea02c376b36da51a2688d5274272c469325e6
Andrew Pinkham <code@andrewsforge.com>
2014-08-28 10:19:25 -0500

    Initial migration formatted to be PEP8 compliant.

--- camelot/roundtable/migrations/0001_initial.py
+++ camelot/roundtable/migrations/0001_initial.py
@@ -9,24 +9,31 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'Knight'
-        db.create_table('roundtable_knight', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=63)),
-        ))
+        db.create_table(
+            'roundtable_knight',
+            (('id',
+              self.gf(
+                  'django.db.models.fields.AutoField')(
+                      primary_key=True)),
+             ('name',
+              self.gf(
+                  'django.db.models.fields.CharField')(
+                      max_length=63)),
+             ))
         db.send_create_signal('roundtable', ['Knight'])
-
 
     def backwards(self, orm):
         # Deleting model 'Knight'
         db.delete_table('roundtable_knight')
 
-
     models = {
         'roundtable.knight': {
             'Meta': {'object_name': 'Knight'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '63'})
-        }
-    }
+            'id': ('django.db.models.fields.AutoField',
+                   [],
+                   {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField',
+                     [],
+                     {'max_length': '63'})}}
 
     complete_apps = ['roundtable']
