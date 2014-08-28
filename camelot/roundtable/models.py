204ee606fac4d63fd9c684d8ac4ed50b92e80873
Andrew Pinkham <code@andrewsforge.com>
2014-08-28 09:52:17 -0500

    Knight model string representation now Python 2 compatible.

--- camelot/roundtable/models.py
+++ camelot/roundtable/models.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8
 from __future__ import unicode_literals
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Knight(models.Model):
     name = models.CharField(max_length=63)
 
