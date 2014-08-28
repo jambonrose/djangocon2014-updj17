dad3b14a280f6f2fb1df93023ab757592ab3ac92
Andrew Pinkham <code@andrewsforge.com>
2014-08-28 09:48:39 -0500

    Created Knight model.

--- camelot/roundtable/models.py
+++ camelot/roundtable/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Knight(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
