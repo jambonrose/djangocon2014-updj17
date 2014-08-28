b26e87ea9f50c60ee07a6bd478fef4fa22516b71
Andrew Pinkham <code@andrewsforge.com>
2014-08-28 12:02:57 -0500

    Formatted third migration file.

--- camelot/roundtable/migrations/0003_auto__add_field_knight_traitor.py
+++ camelot/roundtable/migrations/0003_auto__add_field_knight_traitor.py
@@ -9,22 +9,33 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding field 'Knight.traitor'
-        db.add_column(u'roundtable_knight', 'traitor',
-                      self.gf('django.db.models.fields.BooleanField')(default=False),
-                      keep_default=False)
-
+        db.add_column(
+            u'roundtable_knight',
+            'traitor',
+            self.gf(
+                'django.db.models.fields.BooleanField')(
+                    default=False),
+            keep_default=False)
 
     def backwards(self, orm):
         # Deleting field 'Knight.traitor'
         db.delete_column(u'roundtable_knight', 'traitor')
 
-
     models = {
         u'roundtable.knight': {
             'Meta': {'object_name': 'Knight'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '63'}),
-            'traitor': ('django.db.models.fields.BooleanField', [], {})
+            u'id': (
+                'django.db.models.fields.AutoField',
+                [],
+                {'primary_key': 'True'}),
+            'name': (
+                'django.db.models.fields.CharField',
+                [],
+                {'max_length': '63'}),
+            'traitor': (
+                'django.db.models.fields.BooleanField',
+                [],
+                {})
         }
     }
 
