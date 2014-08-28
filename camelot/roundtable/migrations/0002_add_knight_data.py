e7158329495b3ea555e1b19a4d3e81151c9bce19
Andrew Pinkham <code@andrewsforge.com>
2014-08-28 11:44:32 -0500

    Data migration loads fixture.

--- camelot/roundtable/migrations/0002_add_knight_data.py
+++ camelot/roundtable/migrations/0002_add_knight_data.py
@@ -3,14 +3,12 @@ from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import DataMigration
 from django.db import models
+from django.core.management import call_command
 
 class Migration(DataMigration):
 
     def forwards(self, orm):
-        "Write your forwards methods here."
-        # Note: Don't use "from appname.models import ModelName".
-        # Use orm.ModelName to refer to models in this application,
-        # and orm['appname.ModelName'] for models in other applications.
+        call_command('loaddata', '0002_add_knight_data.json')
 
     def backwards(self, orm):
         "Write your backwards methods here."
