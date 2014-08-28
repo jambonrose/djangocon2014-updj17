ddcc5cb3b975b32deb0a437c105444cf26295ebe
Andrew Pinkham <code@andrewsforge.com>
2014-08-28 15:19:20 -0500

    Generated fourth migration: label Lancelot traitor.

---
+++ camelot/roundtable/migrations/0004_label_lancelot_traitor.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+        # Note: Don't use "from appname.models import ModelName". 
+        # Use orm.ModelName to refer to models in this application,
+        # and orm['appname.ModelName'] for models in other applications.
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+    models = {
+        u'roundtable.knight': {
+            'Meta': {'object_name': 'Knight'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '63'}),
+            'traitor': ('django.db.models.fields.BooleanField', [], {})
+        }
+    }
+
+    complete_apps = ['roundtable']
+    symmetrical = True
