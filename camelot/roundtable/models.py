d686524149a11be5e849b449a49429416ef7005d
Andrew Pinkham <code@andrewsforge.com>
2014-08-29 12:57:19 -0500

    Add False default to traitor BooleanField.

--- camelot/roundtable/models.py
+++ camelot/roundtable/models.py
@@ -7,7 +7,7 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible
 class Knight(models.Model):
     name = models.CharField(max_length=63)
-    traitor = models.BooleanField()
+    traitor = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
