42755823774f4a57849c54d5812e885dfbeee34c
Andrew Pinkham <code@andrewsforge.com>
2014-08-28 20:40:18 -0500

    Use RunPython operation to perform data migration.

--- camelot/roundtable/migrations/0002_add_knight_data.py
+++ camelot/roundtable/migrations/0002_add_knight_data.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models, migrations
+
+
+def add_knight_data(apps, schema_editor):
+    pass
+
+
+def remove_knight_data(apps, schema_editor):
+    pass
 
 
 class Migration(migrations.Migration):
@@ -11,4 +18,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(
+            add_knight_data,
+            reverse_code=remove_knight_data),
     ]
