530da32ac50073ad3efa4209156f5e8d2d4a5e42
Andrew Pinkham <code@andrewsforge.com>
2014-08-29 12:24:13 -0500

    Implemented add_knight_data with fixtures.

--- camelot/roundtable/migrations/0002_add_knight_data.py
+++ camelot/roundtable/migrations/0002_add_knight_data.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models, migrations
+from django.core.management import call_command
 
 
 def add_knight_data(apps, schema_editor):
-    pass
+    call_command('loaddata', 'knight_data.json')
 
 
 def remove_knight_data(apps, schema_editor):
